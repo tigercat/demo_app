341ce130999bd325f8292464f0b09a0991c9abe0
Steve Morss <smorss@catalinacomputing.com>
2011-09-22 12:35:33 -0400

    Fixed a bug - microposts needed fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110922161834) do
+ActiveRecord::Schema.define(:version => 20110922163300) do
 
   create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
